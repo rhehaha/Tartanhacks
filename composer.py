c38055f1dcbf031d2b4f941c61f24ab6337b71b8
tiffanylu23 <tiffanylu68@gmail.com>
2017-02-11 16:24:46 -0500

    some ui edits

--- composer.py
+++ composer.py
@@ -32,6 +32,38 @@ def init(data):
     data.colorBar = []  # List of all the colors in rgb list form
     data.colorPicker = False # If they color picker button is pressed.
     makeColorBar(data)
+
+def drawSplashScreen(screen, data):
+    #draw splash screen
+    data.splashCanvas = pygame.Surface(screen.get_size())
+    data.splashCanvas.fill((0, 0, 0))
+    
+    #draw colors
+    data.pureRed = (255, 0, 0)
+    data.pureOrange = (255, 128, 0)
+    data.pureYellow = (255, 255, 0)
+    data.pureGreen = (102, 204, 0)
+    data.pureBlue = (0, 0, 255)
+    data.purePurple = (102, 0, 204)
+    data.rainbow = [data.pureRed, data.pureOrange, data.pureYellow,data.pureGreen, 
+                    data.pureBlue, data.purePurple]
+    for i in range(6):
+        pygame.draw.ellipse(data.splashCanvas, data.rainbow[i], 
+                                (i*133, -100, 130, 160), 0)
+        pygame.draw.ellipse(data.splashCanvas, data.rainbow[i] ,(i * 133 + 30, 
+                                        30, 70, 150), 0)
+        pygame.draw.ellipse(data.splashCanvas,data.rainbow[i]
+                                ,(i*133+45,150,40,100),0)
+        if i%2 == 0:
+            pygame.draw.ellipse(data.splashCanvas,data.rainbow[i]
+                                ,(i*133+50,225,30,70),0)
+    
+    #draw title
+    titleFont = pygame.font.SysFont('applechancery', 72)
+    title = titleFont.render('c o m p o s e r', True, data.white)
+    data.splashCanvas.blit(title, (210, 230))
+    
+    screen.blit(data.splashCanvas, (0,0))
     
 def drawUI(screen, data):
     data.textCanvas = pygame.Surface(screen.get_size())
@@ -39,6 +71,7 @@ def drawUI(screen, data):
     
     data.white = (250, 250, 250)
     data.lightGray = (200, 200, 200)
+    data.midGray = (150, 150, 150)
     data.black = (0, 0, 0)
     data.brown1 = (215, 160, 87)
     data.brown2 = (180, 128, 60)
@@ -88,6 +121,7 @@ def drawUI(screen, data):
     pygame.draw.polygon(data.textCanvas, data.pink, data.eraserPoints, 0)
     pygame.draw.polygon(data.textCanvas, data.darkPink, data.eraserPoints2, 0)
     #draw audio button
+    pygame.draw.ellipse(data.textCanvas, data.midGray, (608, 540, 61, 59), 0)
     pygame.draw.ellipse(data.textCanvas, data.lightGray, (610,542,57,55), 0)
     pygame.draw.ellipse(data.textCanvas, data.white, (614,546,49,47), 0)
     pygame.draw.polygon(data.textCanvas, data.black, [(620,570), (645,552),
@@ -99,9 +133,9 @@ def drawUI(screen, data):
     pygame.draw.arc(data.textCanvas, data.black, (648,558,12,22), -1*math.pi/2,
                         math.pi/2, 3)
     #draw title
-    titleFont = pygame.font.SysFont('helvetica', 40, True)
+    titleFont = pygame.font.SysFont('applechancery', 42)
     title = titleFont.render('c o m p o s e r', True, data.white)
-    data.textCanvas.blit(title, (280, 545))
+    data.textCanvas.blit(title, (290, 530))
     #draw exit button
     pygame.draw.rect(data.textCanvas, data.black, (740, data.height
         -data.easelBottomH+10, 40, 40), 2)
@@ -363,7 +397,7 @@ def getSounds(screen, data):
     # print(data.colorArr)
     getInstrument(data)
     for x in range(data.width - 20, data.width):
-        print data.instrArr
+        print(data.instrArr)
 
 ####################################
 # use the run function as-is
